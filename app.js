82e189d0879ee88812a35c0097b0c8cca8a302a9
Yukino0102 <24hs106@s.bgu.ac.jp>
2025-01-20 14:04:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-const アイテムリスト = [
+ 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　const アイテムリスト = [
     "星のペンダント",
     "幸運のコイン",
     "ラッキーストーン",
     "虹のブレスレット",
     "金運アップの財布"
-];
-
-function アイテムを表示() {
-    const ランダムなインデックス = Math.floor(Math.random() * アイテムリスト.length);
-    const アイテム要素 = document.getElementById("item");
-    
-    // ラッキーアイテムを表示
-    アイテム要素.innerText = アイテムリスト[ランダムなインデックス];
-    
     // スタイルを変更してピンク色にする
     アイテム要素.style.color = "pink";
 }
