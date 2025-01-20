ab7e49231e82d6c8f129a925006f900776417d2e
Yukino0102 <24hs106@s.bgu.ac.jp>
2025-01-20 14:33:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 pink {
     color: pink; /* テキストをピンク色にする */
 }
+const アイテムリスト = [
+    "星のペンダント",
+    "幸運のコイン",
+    "ラッキーストーン",
+    "虹のブレスレット",
+    "金運アップの財布"
+];
+
+function アイテムを表示() {
+    const ランダムなインデックス = Math.floor(Math.random() * アイテムリスト.length);
+    const アイテム = アイテムリスト[ランダムなインデックス];
+    const itemDiv = document.getElementById("item");
+    itemDiv.innerText = アイテム;
+
+    // アイテムに応じてクラスを追加
+    if (アイテムリスト.includes(アイテム)) {
+        itemDiv.className = "pink"; // ピンク色のクラスを追加
+    }
+}
